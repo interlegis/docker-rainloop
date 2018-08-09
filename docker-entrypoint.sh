27be31e41f311b77da598a306a8fc2ee006071b6
Fabio Rauber <fabiorauber@gmail.com>
2018-08-09 09:57:15 -0300

    No need to change permissions of every file at startup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-chown -R apache:apache ${RAINLOOP_HOME}
-
-sh -c "find . -type d -exec chmod 755 {} \;"
-sh -c "find . -type f -exec chmod 644 {} \;"
-
 exec /usr/sbin/httpd -D FOREGROUND
